cb80ab556d8364e7b36cce8ca05a30cd9b702ef9
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-03-02 14:43:22 +0100

    final update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 // restituire nome
 var name = prompt('inserisci il tuo nome:');
-var surname = prompt('inserisci il tuo cognome:');;
+var surname = prompt('inserisci il tuo cognome:');
 var color = prompt('inserisci il tuo colore preferito:');
 // console.log(name, surname, color);
 
-document.getElementById('name').innerHTML = name;
-document.getElementById('surname').innerHTML = surname;
-document.getElementById('color').innerHTML = color;
+document.getElementById('string').innerHTML = name + surname + color + '19';
